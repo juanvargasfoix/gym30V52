32a648df8a377241a33c05821f2895c8e8be7c6d
Juan Vargas <juan@arenaplay.ar>
2026-02-06 18:31:40 -0300

    Cambios ultimos

--- components/CompetenceMap.tsx
+++ components/CompetenceMap.tsx
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import React, { useState, useEffect } from 'react';
 import { User, Skill, Company, UserProgress, Kudo, FlexArea } from '../types';
 import { calculateRank, ranks } from '../utils/data';
